2298db2e3e87bf79f6368d6cbd925ab5de56553d
Daniel Delchev <daniel.delchev@yahoo.com>
2025-01-12 23:29:33 +0200

    Adieau problem solution

--- Week 4 Problem Set - Libraries/02/adieau.py
+++ Week 4 Problem Set - Libraries/02/adieau.py
@@ -2,9 +2,9 @@
 # [x] create a funct
 # [x] start prompting the user for input
 # [x] store the names in a list
-# [] separate the names properly
-# [] bind adieu to the name
-# [] print the names with adieu binded to them
+# [x] separate the names properly
+# [x] bind adieu to the name
+# [x] print the names with adieu binded to them
 
 
 import inflect

e4c370a5d7dcf583c178d1f4430046db3aa6d093
Daniel Delchev <daniel.delchev@yahoo.com>
2025-02-16 21:55:47 +0200

    Update user input function

--- Final Project/project.py
+++ Final Project/project.py
@@ -7,65 +7,77 @@
 
 def main():
     user_data = get_user_input()
-    calculate_calories(user_data)
+
+    # if user_data["goal"] == 'Loose weight':
+    #     calorie_deficit(user_data)
+    # elif user_data['goal'] == 'Gain weight':
+    #     calorie_surplus(user_data)
+    # elif user_data['goal'] == 'Maintain weight':
+    #     calculate_calories(user_data)
+        
+
+    # calculate_calories(user_data)
 
 
 def get_user_input():
-    activity = {
+    activities = {
         "Sedentary (little to no exercise)": float(1.2),
         "Light exercise (1-3 days per week)": float(1.375),
         "Moderate exercise (3-5 days per week)" : float(1.55),
         "Heavy exercise (6-7 days per week)" : float(1.725),
         "Very heavy exercise (Twice per day,intense)" :float(1.9)
     }
+
+    goals = ['1) Loose weight' ,'2) Gain weight', '3) Maintain weight']
+
+    user_data = {}
     
     try:
         age = int(input('Age: '))
         weight = int(input('Weight: '))
         height = int(input('Height: '))
+
+        
+
+        print('Please select your goal.')
+
+        for goal in goals:
+            print("  " + goal)
  
+
+        user_goal = int(input("Goal: "))
+
+        if user_goal == 1:
+            goal = 'Loose weight'
+        elif user_goal == 2:
+            goal = 'Gain weight'
+        elif user_goal == 3:
+            goal = 'Maintain weight'
+
+
         print("What is your activity level ?")
     
-        for index, key in enumerate(activity.keys(), start=1):
+        for index, key in enumerate(activities.keys(), start=1):
             print(f'  {index}) {key}')
+        
+        
 
         user_activity = int(input("Please select the number of the activity level: "))
 
         if user_activity == 1:
-            return{
-                'age': age,
-                'weight': weight,
-                'height': height,
-                'activity': activity['Sedentary (little to no exercise)']
-            }
+            activity = "Sedentary"
         elif user_activity == 2:
-            return{
-                'age': age,
-                'weight': weight,
-                'height': height,
-                'activity': activity['Light exercise (1-3 days per week)']
-            }
+            activity = "Light exercise"
         elif user_activity == 3:
-            return{
-                'age': age,
-                'weight': weight,
-                'height': height,
-                'activity': activity['Moderate exercise (3-5 days per week)']
-            }
+            activity = 'Moderate exercise'
         elif user_activity == 4:
-            return{
-                'age': age,
-                'weight': weight,
-                'height': height,
-                'activity': activity['Heavy exercise (6-7 days per week)']
-            }
+            activity =  "Heavy exercise"
         elif user_activity == 5:
-            return{
-                'age': age,
-                'weight': weight,
-                'height': height,
-                'activity': activity['Very heavy exercise (Twice per day,intense)']
-            }
+            activity = "Very heavy exercise"
+
+        print(age, weight, height, goal, activity)
+        
+        
     except ValueError:
         print('Invalid input !')
 
@@ -77,9 +89,14 @@ def calculate_calories(input):
 
     total_calories = base_metabolic_rate * activity
 
-    print(f'Your daily calories to maintain weight are: {int(total_calories)} kcals')
+    print(f'Your Total Daily Energy Expenditure (TDEE) is : {int(total_calories)} kcals')
+    
+    
+def calorie_deficit():
+    ...
 
-    input()
+def calorie_surplus():
+    ...
     
 
        
